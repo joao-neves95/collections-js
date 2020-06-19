be6926755eaa6a994301ad2efcdc11bc3c2c3f47
João Neves <joao95neves@gmail.com>
2020-06-19 20:11:34 +0100

    Added the Stack

---
+++ test/spec/stack.spec.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019-2020 João Pedro Martins Neves - All Rights Reserved.
+ *
+ * js.system.collections is licensed under the MIT license,
+ * located in the root of this project, under the name "LICENSE.md".
+ *
+ */
+
+const { Stack } = require( '../../dist/js.system.collections' );
+
+describe( 'The Stack', function() {
+
+  beforeAll( function() {
+    global.myStack = new Stack();
+  } );
+
+  it( 'push multiple values', function() {
+    global.myStack.push( '1' );
+    global.myStack.push( '2' );
+    global.myStack.push( '3' );
+    expect( global.myStack ).toBeDefined();
+    expect( global.myStack.length ).toEqual( 3 );
+    expect( global.myStack.peekAll().length ).toEqual( 3 );
+  });
+
+  it( 'peek the most recent element', function() {
+    expect( global.myStack.peek() ).toBeDefined();
+    expect( global.myStack.peek() ).toEqual( '3' );
+  });
+
+  it( 'pop the most recent element', function() {
+    const mostRecentElem = global.myStack.pop();
+    expect( mostRecentElem ).toEqual( '3' );
+
+    expect( global.myStack.length ).toEqual( 2 );
+    expect( global.myStack.peekAll().length ).toEqual( 2 );
+  });
+
+});
